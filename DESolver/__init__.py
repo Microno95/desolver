f327ffa0af902351560ac995c8c81c786b5ab725
Microno95 <ekin.ozturk@mail.utoronto.ca>
2016-03-12 18:48:38 -0500

    Remove duplicated directory

--- DESolver/__init__.py
+++
@@ -1,7 +0,0 @@
-__all__ = ["odesystem", "bisectroot", "seval", "explicitrk4",
-		   "explicitmidpoint", "implicitmidpoint", "heuns", "backeuler", 
-		   "foreuler", "eulertrap", "adaptiveheuneuler", 
-		   "sympforeuler", "init_namespace", "VariableMissing",
-		   "LengthError", "warning", "odesystem"]
-
-from .differentialsystem import *
