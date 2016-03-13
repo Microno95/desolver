b97c17e5694d0345d68a854f1a3af4219e8cf9b0
Microno95 <ekin.ozturk@mail.utoronto.ca>
2016-03-13 13:03:26 -0400

    Made the user interface more modular, included the ability to more easily modify the time limits and stepsize of the system. Also corrected a problem with switched parameters in a for loop.

--- setup.py
+++ setup.py
@@ -3,34 +3,34 @@
 from distutils.core import setup
 
 setup(name='DESolver',
-      version='1.04',
+      version='1.05',
       description='Differential Equation System Solver',
       author='Ekin Ozturk',
       author_email='ekin.ozturk@mail.utoronto.ca',
-	  install_requires=['numpy'],
-	  license='MIT',
+      install_requires=['numpy'],
+      license='MIT',
       url='https://github.com/Microno95/desolver',
       packages=['desolver'],
-	  classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
+      classifiers=[
+          # How mature is this project? Common values are
+          #   3 - Alpha
+          #   4 - Beta
+          #   5 - Production/Stable
+          'Development Status :: 3 - Alpha',
 
-        # Indicate who your project is intended for
-        'Intended Audience :: Developers',
-        'Topic :: Scientific/Engineering :: Mathematics',
+          # Indicate who your project is intended for
+          'Intended Audience :: Developers',
+          'Topic :: Scientific/Engineering :: Mathematics',
 
-        # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: MIT License',
+          # Pick your license as you wish (should match "license" above)
+          'License :: OSI Approved :: MIT License',
 
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-		'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-    ],
-     )
+          # Specify the Python versions you support here. In particular, ensure
+          # that you indicate whether you support Python 2, Python 3 or both.
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+      ],
+      )
