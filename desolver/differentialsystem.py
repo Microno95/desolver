881a47cdd97841dbd25feba9a62c2c71a1c4b188
Microno95 <ekin.ozturk@mail.utoronto.ca>
2016-04-09 02:53:44 -0400

    ETA estimation is shown to be accurate, corrected a minor issue with timestepping during integration where the timestep would become miniscule very close to the end of the integration time.
    Also included thorough docstrings for each sub-function in the OdeSystem class with small tidbits of advice.

--- desolver/differentialsystem.py
+++ desolver/differentialsystem.py
@@ -470,8 +470,30 @@ class LengthError(Exception):
 
 class OdeSystem:
     """Ordinary Differential Equation class. Designed to be used with a system of ordinary differential equations."""
-
     def __init__(self, n=(1,), equ=(), y_i=(), t=(0, 0), savetraj=0, stpsz=1.0, eta=0, relerr=4e-16, **consts):
+        """Initialises the system to the parameters passed or to default values.
+
+        Keyword arguments:
+        n: Specifies the dimensions of the system in the form of a tuple.
+           Can be arbitrary as long as the values are integral.
+        equ: Specifies the list of differential equations. Use in the form of strings where t and y_{} are the variables.
+             The curly braces are to be replaced by values that range from 0 to k where
+             k = total_number_of_equations - 1.
+             NOTE: y_0 will be the first equation, y_1 the second, and so on.
+        y_i: Specifies the initial conditions of each equation in the order of equations that are passed.
+        t: A tuple of the form (initial time, final time) aka the integration limits.
+        savetraj: Set to True or False to specify whether or not the trajectory of the
+                  integration should be recorded.
+        stpsz: Sets the step-size for the integration, choose a value that is slightly less than the highest frequency
+               changes in value of the solutions to the equations.
+        eta: Set to True or False to specify whether or not the integration process should return an eta,
+             current progress and simple information regarding step-size and current time.
+             NOTE: This may slow the integration process down as the process of outputting
+                   these values create overhead.
+        relerr: Denotes the target relative global error. Useful for adaptive methods.
+
+        Variable-length arguments:
+        consts: Arbitrary set of keyword arguments that define the constants to be used in the system."""
         if len(equ) > len(y_i):
             raise LengthError("There are more equations than initial conditions!")
         elif len(equ) < len(y_i):
@@ -504,21 +526,40 @@ class OdeSystem:
         self.eqnum = len(equ)
 
     def chgendtime(self, t):
+        """Changes the final time for the integration of the ODE system
+
+        Required arguments:
+        t: Denotes the final time."""
         self.t1 = float(t)
         if not (abs(self.t0) < abs(self.t) < abs(self.t1)):
             self.t = self.t0
 
     def chgbegtime(self, t):
+        """Changes the initial time for the integration of the ODE system.
+
+        Required arguments:
+        t: Denotes the initial time."""
         self.t0 = float(t)
         if not (abs(self.t0) < abs(self.t) < abs(self.t1)):
             self.t = self.t0
 
     def chgcurtime(self, t):
+        """Changes the current time for the integration of the ODE system.
+
+        Required arguments:
+        t: Denotes the current time"""
         self.t = float(t)
 
     def chgtime(self, t=()):
+        """Alternate interface for changing current, beginning and end times.
+
+        Keyword arguments:
+        t:  -- A length of 1 denotes changes to current time.
+            -- A length of 2 denotes changes to beginning and end times in that order.
+            -- A length of 3 denotes changes to all three times in order of current, beginning and end.
+            -- A length larger than 3 will behave the same as above and ignore values beyond the 3rd index."""
         if len(t) == 1:
-            warning("You have passed an array that only contains one element, "
+            warning("You have passed a tuple that only contains one element, "
                     "this will be taken as the current time.")
             self.t = t[0]
         elif len(t) == 2:
@@ -538,9 +579,26 @@ class OdeSystem:
             warning("You have passed an array that is empty, this doesn't make sense.")
 
     def setstepsize(self, h):
+        """Sets the step size that will be used for the integration.
+
+        Required arguments:
+        dt: Step size value. For systems that grow exponentially choose a smaller value, for oscillatory systems choose
+            a value slightly less than the highest frequency of oscillation. If unsure, use an adaptive method in the
+            list of available methods (view by calling availmethods()) followed by setmethod(), and finally call
+            setrelerr() with the keyword argument auto_calc_dt set to True for an approximately good step size."""
         self.dt = h
 
     def setrelerr(self, relative_err, auto_calc_dt=0):
+        """Sets the value for target relative global error, especially useful for adaptive methods.
+
+        Required arguments:
+        relative_err: Generally a float less than or equal to 1, do NOT set to 0 as that is an impossible goal
+                      and will cause an infinite loop in adaptive methods.
+
+        Keyword arguments:
+        auto_calc_dt: if set to true, will use knowledge of the order of the method chosen in order to estimate
+                      a good step size.
+                      NOTE: This will not necessarily be the optimal step size, but will suffice in most cases."""
         self.relative_error_bound = relative_err
         if auto_calc_dt:
             alt_h = float(self.t1 - self.t) * (self.relative_error_bound ** methods_inv_order[self.method])
@@ -550,10 +608,15 @@ class OdeSystem:
 
     @staticmethod
     def availmethods():
+        """Prints and then returns a dict of methods of integration that are available."""
         print(available_methods.keys())
         return available_methods
 
     def setmethod(self, method):
+        """Sets the method of integration.
+
+        Required arguments:
+        method: String that denotes the key to one of the available methods in the dict() returned by availmethods()."""
         if method in available_methods.keys():
             self.method = method
         else:
@@ -561,6 +624,16 @@ class OdeSystem:
                   "call availmethods() to see what these are")
 
     def addequ(self, eq, ic):
+        """Adds an equation to an already defined system.
+
+        Required arguments:
+        eq: A list of strings that use y_{} and t as the integration variables where the curly braces should be
+            replaced with the index of the equation being referenced starting from 0 as the first equation.
+            NOTE: Referencing an equation that does not exist, ie. y_10 for a system with 10 equations will cause
+                  the integrator to fail.
+        ic: A list of values for each eq in order of the equations entered. If the dimension of the system is set
+            to be larger than scalars then an initial condition with fewer specified values will be extended to the
+            necessary dimensions. This may cause unexpected results and it is better to specify all the coefficients."""
         if eq and ic:
             for equation, icond in zip(eq, ic):
                 self.equ.append(equation)
@@ -572,6 +645,12 @@ class OdeSystem:
             self.t = self.t0
 
     def delequ(self, indices):
+        """Removes (an) equation(s) at (a) given ind(ex)(ices) along with its corresponding initial values.
+
+        Required arguments:
+        indices: A list of integers that denote the equations to remove from the system. Will throw an error if
+                 there are more equations to be removed or if there is an index specified that exceeds the
+                 number of equations that exist."""
         if len(indices) > self.eqnum:
             raise LengthError("You've specified the removal of more equations than there exists!")
         for i in indices:
@@ -581,30 +660,39 @@ class OdeSystem:
         self.eqnum -= len(indices)
 
     def showequ(self):
+        """Prints the equations that have been entered for the system.
+
+        Returns the equations themselves as a list of strings."""
         for i in range(self.eqnum):
             print("dy_{} = ".format(i) + self.equ[i])
         return self.equ
 
     def numequ(self):
+        """Prints then returns the number of equations in the system"""
         print(self.eqnum)
         return self.eqnum
 
     def initcond(self):
+        """Prints the initial conditions of the system"""
         for i in range(self.eqnum):
             print("y_{}({}) = {}".format(i, self.t0, self.y[i]))
         return self.y
 
     def finalcond(self, p=1):
+        """Prints the final state of the system.
+
+        Identical to initial conditions if the system has not been integrated"""
         if p:
             for i in range(self.eqnum):
                 print("y_{}({}) = {}".format(i, self.t1, self.soln[i][-1]))
         return self.soln
 
     def showsys(self):
+        """Prints the equations, initial conditions, final states, time limits and defined constants in the system."""
         for i in range(self.eqnum):
-            print("Equation {}\ny_{}({}) = {}\ndy_{} = {}\ny_{}({}) = {}".format(i, i, self.t0, self.y[i], i,
-                                                                                 self.equ[i], i, self.t,
-                                                                                 self.soln[i][-1]))
+            print("Equation {}\ny_{}({}) = {}\ndy_{} = {}\ny_{}({}) = {}\n".format(i, i, self.t0, self.y[i], i,
+                                                                                   self.equ[i], i, self.t,
+                                                                                   self.soln[i][-1]))
         if self.consts:
             print("The constants that have been defined for this system are: ")
             print(self.consts)
@@ -612,14 +700,28 @@ class OdeSystem:
               "t0 = {}, t1 = {}, t_current = {}, step_size = {}".format(self.t0, self.t1, self.t, self.dt))
 
     def addconsts(self, **additional_constants):
+        """Takes an arbitrary list of keyword arguments to add to the list of available constants.
+
+        Variable-length arguments:
+        additional_constants: A dict containing constants and their corresponding values."""
         self.consts.update({k: numpy.resize(additional_constants[k], self.dim) for k in additional_constants})
 
     def remconsts(self, **constants_removal):
+        """Takes an arbitrary list of keyword arguments to remove from the list of available constants.
+
+        Variable-length arguments:
+        additional_constants: A tuple or list containing the names of the constants to remove.
+                              The names must be denoted by strings."""
         for i in constants_removal:
             if i in self.consts.keys():
                 del self.consts[i]
 
     def chgdim(self, m=None):
+        """Changes the dimensions of the system.
+
+        Keyword arguments:
+        m: Takes a tuple that describes the dimensions of the system. For example, to integrate 3d vectors one would
+        pass (3,3)."""
         if m is not None:
             if isinstance(m, float):
                 raise ValueError('The dimension of a system cannot be a float')
@@ -632,13 +734,34 @@ class OdeSystem:
             self.soln = [[numpy.resize(i, m)] for i in self.soln[:-1]]
             self.soln.append(solntime)
 
+    def recordtraj(self, b=None):
+        """Sets whether or not the trajectory of the system will be recorded.
+
+        Keyword arguments:
+        b: A boolean value that denotes if the trajectory should be recorded.
+           1 - implies record; 0 - implies don't record. If b is None, then this will invert the sate from recording
+           trajectories to not recording trajectories and vice versa."""
+        if b is None:
+            self.traj = (not self.traj)
+        else:
+            self.traj = b
+
     def reset(self, t=None):
+        """Resets the system to a previous time.
+
+        Keyword arguments:
+        t: If specified after the system has recorded the trajectory of the system during integration, then this will
+           revert to that time. Otherwise the reversion of the system will be to its state prior to integration."""
         if t is not None:
-            k = numpy.array(self.soln[-1])
-            ind = numpy.argmin(numpy.square(numpy.subtract(k, t)))
-            for i, k in enumerate(self.soln):
-                self.soln[i] = list(numpy.delete(k, numpy.s_[ind + 1:], axis=0))
-            self.t = t
+            if self.traj:
+                k = numpy.array(self.soln[-1])
+                ind = numpy.argmin(numpy.square(numpy.subtract(k, t)))
+                for i, k in enumerate(self.soln):
+                    self.soln[i] = list(numpy.delete(k, numpy.s_[ind + 1:], axis=0))
+                self.t = t
+            else:
+                warning('Trajectory has not been recorded for prior integration, cannot revert to t = {}\nPlease '
+                        'call reset() and record trajectory by calling recordtraj() before integrating'.format(t))
         else:
             for i in range(self.eqnum + 1):
                 if i < self.eqnum:
@@ -648,6 +771,13 @@ class OdeSystem:
             self.t = 0
 
     def integrate(self, t=None):
+        """Integrates the system to a specified time.
+
+        Keyword arguments:
+        t: If t is specified, then the system will be integrated to time t.
+           Otherwise the system will integrate to the specified final time.
+           NOTE: t can be negative in order to integrate backwards in time, but use this with caution as this
+                 functionality is slightly unstable."""
         method = self.method
         if method is None:
             method = available_methods["Explicit Runge-Kutta 4"]
@@ -681,8 +811,10 @@ class OdeSystem:
                     time_remaining[0] = tm.perf_counter()
                 if abs(heff[0] + self.t) - abs(tf) > abs(tf) * 4e-15:
                     heff[0] = (tf - self.t) * 0.5
+                elif heff[1] == 0 and heff[0] == 0:
+                    break
                 elif heff[0] == 0:
-                    heff[0] = 1e-15
+                    heff[0] = (tf - self.t) * 0.03125
                 method(self.equ, vardict, soln, heff, self.relative_error_bound)
                 self.t = vardict['t']
                 if self.traj:
