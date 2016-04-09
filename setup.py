5d5e46721d2ac1000e8534d3de4bdc9e4097eb87
Microno95 <ekin.ozturk@mail.utoronto.ca>
2016-04-09 02:54:13 -0400

    ETA estimation is shown to be accurate, corrected a minor issue with timestepping during integration where the timestep would become miniscule very close to the end of the integration time.
    Also included thorough docstrings for each sub-function in the OdeSystem class with small tidbits of advice.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='DESolver',
-      version='1.1.23',
+      version='1.1.24',
       description='Differential Equation System Solver',
       author='Ekin Ozturk',
       author_email='ekin.ozturk@mail.utoronto.ca',
