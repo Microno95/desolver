0968caccf7ca9a241b596ee73dedf8759c98b571
Ekin Ozturk <ekin.ozturk@mail.utoronto.ca>
2016-12-27 20:26:23 +0300

    Updated version number in setup.py

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ SOFTWARE.
 from distutils.core import setup
 
 setup(name='DESolver',
-      version='1.1.40',
+      version='1.1.41',
       description='Differential Equation System Solver',
       author='Ekin Ozturk',
       author_email='ekin.ozturk@mail.utoronto.ca',
