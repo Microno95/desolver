3f1df9b9456079ddd1cb3bb1a15edcb1196eaf75
Microno95 <ekin.ozturk@mail.utoronto.ca>
2017-01-27 12:31:02 -0500

    Updated Version to 1.3.2

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ SOFTWARE.
 from setuptools import setup
 
 setup(name='DESolver',
-      version='1.3.1',
+      version='1.3.2',
       description='Differential Equation System Solver',
       author='Ekin Ozturk',
       author_email='ekin.ozturk@mail.utoronto.ca',
