b552443fa992e430886b326a229a9754890262ea
Ekin Ozturk <ekin.ozturk@mail.utoronto.ca>
2017-01-29 20:11:12 -0500

    Corrected minor msitake of referring to numpy as np instead of numpy.

--- desolver/differentialsystem.py
+++ desolver/differentialsystem.py
@@ -1085,7 +1085,7 @@ class OdeSystem:
                 if self.traj:
                     soln[-1].append(vardict['t'])
                 else:
-                    soln = [np.array([i[-1]]) for i in soln[:-1]]
+                    soln = [numpy.array([i[-1]]) for i in soln[:-1]]
                     soln.append([vardict['t']])
                 if eta:
                     temp_time = 0.4 * time_remaining[1] + (((tf - self.t) / heff[0]) *
