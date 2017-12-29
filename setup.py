964424b2820b07d85d42fde80e755a06c5e88d3b
Microno95 <ekin.ozturk@mail.utoronto.ca>
2017-12-29 18:55:26 +0300

    Updated version number to reflect the new version on PyPi

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ SOFTWARE.
 from setuptools import setup
 
 setup(name='DESolver',
-      version='1.5.0',
+      version='1.5.1',
       description='Differential Equation System Solver',
       author='Ekin Ozturk',
       author_email='ekin.ozturk@mail.utoronto.ca',
