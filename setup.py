5938cccd0fcf2c280a2aae9da7ce86ea117514b4
Ekin Ozturk <ekin.ozturk@mail.utoronto.ca>
2019-12-30 17:08:40 +0300

    Updated pypi package version.

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='desolver',
-      version='3.0.0b1',
+      version='3.0.0b2',
       description='Differential Equation System Solver',
       author='Ekin Ozturk',
       author_email='ekin.ozturk@mail.utoronto.ca',
