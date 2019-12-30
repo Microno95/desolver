714ca43c16974ef52cdb4ef0c573739c4fc16340
Ekin Ozturk <ekin.ozturk@mail.utoronto.ca>
2019-12-30 16:57:02 +0300

    Removed redundant test scripts.

--- desolver/backend/tests/test_numpy_backend.py
+++
@@ -1,7 +0,0 @@
-# from backend_test_utility import test_backend
-
-# import os
-# import numpy as np
-
-# if __name__ == "__main__":
-#     np.testing.run_module_suite()
--- desolver/backend/tests/test_torch_backend.py
+++
@@ -1,8 +0,0 @@
-# from backend_test_utility import test_backend
-
-# import os
-# os.environ['DES_BACKEND'] = 'torch'
-# import numpy as np
-
-# if __name__ == "__main__":
-#     np.testing.run_module_suite()
