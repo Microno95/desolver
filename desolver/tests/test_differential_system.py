e4ca3c9d003e97180d1571f52eb74e130ab29ec2
Ekin Ozturk <ekin.ozturk@esa.int>
2020-01-14 17:30:35 +0100

    Renamed test and added more comprehensive DenseOutput test.

--- desolver/tests/test_differential_system.py
+++ desolver/tests/test_differential_system.py
@@ -98,7 +98,35 @@ def test_getter_setters():
 
         assert('k' not in a.consts.keys())
         
-        a = de.OdeSystem(rhs, y0=y_init, dense_output=True, t=(0, 2*D.pi), dt=0.01, rtol=D.epsilon()**0.5, atol=D.epsilon()**0.5)
+def test_integration_and_representation():
+    
+    for ffmt in D.available_float_fmt():
+        D.set_float_fmt(ffmt)
+
+        print("Testing {} float format".format(D.float_fmt()))
+
+        de_mat = D.array([[0.0, 1.0],[-1.0, 0.0]])
+
+        @de.rhs_prettifier("""[vx, -x+t]""")
+        def rhs(t, state, k, **kwargs):
+            return de_mat @ state + D.array([0.0, t])
+
+        def analytic_soln(t, initial_conditions):
+            c1 = initial_conditions[0]
+            c2 = initial_conditions[1] - 1
+            
+            return D.stack([
+                c2 * D.sin(D.to_float(t)) + c1 * D.cos(D.to_float(t)) + t,
+                c2 * D.cos(D.to_float(t)) - c1 * D.sin(D.to_float(t)) + 1
+            ])
+
+        def kbinterrupt_cb(ode_sys):
+            if ode_sys[-1][0] > D.pi:
+                raise KeyboardInterrupt("Test Interruption and Catching")
+
+        y_init = D.array([1., 0.])
+
+        a = de.OdeSystem(rhs, y0=y_init, dense_output=True, t=(0, 2*D.pi), dt=0.01, rtol=D.epsilon()**0.5, atol=D.epsilon()**0.5, constants=dict(k=1.0))
         
         a.integrate()
 
@@ -107,12 +135,9 @@ def test_getter_setters():
             print(repr(a))
             assert(D.max(D.abs(a.sol(a.t[0]) - y_init)) <= 8*D.epsilon()**0.5)
             assert(D.max(D.abs(a.sol(a.t[-1]) - analytic_soln(a.t[-1], y_init))) <= 8*D.epsilon()**0.5)
+            assert(D.max(D.abs(a.sol(a.t).T - analytic_soln(a.t, y_init))) <= 8*D.epsilon()**0.5)
         except:
             raise
-            
-        
-        print("{} backend test passed successfully!".format(D.backend()))
-        
         
 if __name__ == "__main__":
     np.testing.run_module_suite()
