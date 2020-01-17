906aa00ed940e642d78227623312df0bac2fa998
Ekin Ozturk <ekin.ozturk@esa.int>
2020-01-17 11:44:15 +0100

    Changed master file in conf.py to index.rst

--- docs/conf.py
+++ docs/conf.py
@@ -37,6 +37,9 @@ extensions = [
     "sphinx.ext.napoleon"
 ]
 
+# Set master file to index.rst
+master_doc = 'index'
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
