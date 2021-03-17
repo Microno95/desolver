d717a37a82feacecdcc373750e0413840535186c
Ekin Ozturk <ekin.ozturk@mail.utoronto.ca>
2021-03-17 16:46:36 +0000

    Moved integration_schemes to explicit

--- desolver/integrators/__init__.py
+++ desolver/integrators/__init__.py
@@ -1,10 +1,11 @@
 from .integrator_template import *
 from .integrator_types import *
-from .integration_schemes import *
+from .explicit_integration_schemes import *
+# from .imlicit_integration_schemes import *
 
 __available_methods = dict()
 
-__integration_methods__ = [
+__explicit_integration_methods__ = [
     RK1412Solver,
     RK108Solver,
     RK8713MSolver,
@@ -21,9 +22,16 @@ __integration_methods__ = [
     ABAs5o6HSolver
 ]
 
+__implicit_integration_methods__ = [
+]
+
 __available_methods.update(dict(
-    [(func.__name__, func) for func in __integration_methods__ if hasattr(func, "__alt_names__")] +
-    [(alt_name, func)      for func in __integration_methods__ if hasattr(func, "__alt_names__") for alt_name in func.__alt_names__]))
+    [(func.__name__, func) for func in __explicit_integration_methods__ if hasattr(func, "__alt_names__")] +
+    [(alt_name, func)      for func in __explicit_integration_methods__ if hasattr(func, "__alt_names__") for alt_name in func.__alt_names__]))
+
+__available_methods.update(dict(
+    [(func.__name__, func) for func in __implicit_integration_methods__ if hasattr(func, "__alt_names__")] +
+    [(alt_name, func)      for func in __implicit_integration_methods__ if hasattr(func, "__alt_names__") for alt_name in func.__alt_names__]))
 
 def available_methods(names=True):
     if names:
