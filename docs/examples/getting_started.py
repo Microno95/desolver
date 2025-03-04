f1720c9f8c084c9317fe24a13bf5942fc55800ea
Ekin Ozturk <ekin.ozturk17@imperial.ac.uk>
2025-03-04 15:58:30 +0000

    Updated numpy example notebooks

--- docs/examples/getting_started.py
+++ docs/examples/getting_started.py
@@ -1,5 +1,5 @@
 import desolver as de
-import desolver.backend as D
+import numpy as np
 
 @de.rhs_prettifier(
     equ_repr="[vx, -k*x/m]",
@@ -13,11 +13,11 @@ $$
 """
 )
 def rhs(t, state, k, m, **kwargs):
-    return D.array([[0.0, 1.0], [-k/m,  0.0]])@state
+    return np.array([[0.0, 1.0], [-k/m,  0.0]])@state
 
-y_init = D.array([1., 0.])
+y_init = np.array([1., 0.])
 
-a = de.OdeSystem(rhs, y0=y_init, dense_output=True, t=(0, 2*D.pi), dt=0.01, rtol=1e-9, atol=1e-9, constants=dict(k=1.0, m=1.0))
+a = de.OdeSystem(rhs, y0=y_init, dense_output=True, t=(0, 2*np.pi), dt=0.01, rtol=1e-9, atol=1e-9, constants=dict(k=1.0, m=1.0))
 
 print(a)
 
@@ -29,4 +29,4 @@ print("If the integration was successful and correct, a[0].y and a[-1].y should 
 print("a[0].y  = {}".format(a[0].y))
 print("a[-1].y = {}".format(a[-1].y))
 
-print("Maximum difference from initial state after one oscillation cycle: {}".format(D.max(D.abs(a[0].y-a[-1].y))))
+print("Maximum difference from initial state after one oscillation cycle: {}".format(np.max(np.abs(a[0].y-a[-1].y))))
